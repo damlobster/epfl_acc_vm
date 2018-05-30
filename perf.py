89389bec499460e882f269cd5f11b567b1e717c3
Martin <damien.martin@epfl.ch>
2018-05-30 13:32:18 +0200

    pass heap size argument to VM

--- perf.py
+++ perf.py
@@ -8,7 +8,7 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description='Do performance tests of L3 vm')
-parser.add_argument('-m', dest='heap', default=1000000, help="Heap size in bytes")
+parser.add_argument('-m', dest='heap', default="1000000", help="Heap size in bytes")
 parser.add_argument('-n', dest='n', default=1, help='Number of iterations', type=int)
 parser.add_argument('-b', dest='make', nargs='*', default='', help='Arguments passed to make')
 parser.add_argument('-s', "--silent", dest='silent', action="store_true", default=False, help='Redirect output to /dev/null')
@@ -21,7 +21,10 @@ if 0 != os.system('make ' + ' '.join(args.make)):
 times = []
 for i in range(args.n):
     start = time()
-    os.system('echo {} | bin/vm test/{}.asm {}'.format(' '.join(args.l3[1:]), args.l3[0], ' > /dev/null' if args.silent else ''))
+    cmd = 'echo {} | bin/vm {} test/{}.asm {}'.format(' '.join(args.l3[1:]), '-m ' + args.heap, args.l3[0], ' > /dev/null' if args.silent else '')
+    if not args.silent:
+        print(cmd)
+    os.system(cmd)
     times += [time()-start]
     print('.', end='', flush=True)
 
